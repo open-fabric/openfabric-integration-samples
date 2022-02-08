48792dfa52d3fe58f0a89870acdfb338f5ed31f3
duonganhthu <duonganhthu43@gmail.com>
2022-02-08 19:01:30 +0700

    clean up script

--- start.sh
+++ start.sh
@@ -1,47 +1,50 @@
 
-MODE=dev
-API_DEV=https://api.dev.openfabric.co
-API_SANDBOX=https://api.sandbox.openfabric.co
-API_URL=$API_DEV
 
 function networkDown() {
     docker-compose down --rmi all
     # docker-compose down
 }
 
-function setupEnviroment() {
-    PS3='Please enter your enviroment: '
-    options=("DEV" "SANDBOX")
-    select opt in "${options[@]}"
-    do
-        case $opt in
-            "DEV")
-                echo "you choose DEV"
-                MODE=DEV
-                API_URL=$API_DEV
-                break
-            ;;
-            "SANDBOX")
-                echo "you choose SANDBOX"
-                MODE=SANDBOX
-                API_URL=$API_SANDBOX
-                break
-            ;;
-            *) echo "invalid option $REPLY";;
-        esac
-    done
-}
-
 function proxyAccountServer() {
     URL=$(curl $(docker port ngrok-account-server 4040)/api/tunnels/command_line | jq -r '.public_url')
     URL+="/account-create-transaction"
-    echo "$URL"
-    echo $API_URL
+    
+    apiURL=$(grep 'OF_API_URL' .env)
+    OF_API_URL=${apiURL#*=}
+    
+    ofAuthUrl=$(grep 'OF_AUTH_URL' .env)
+    OF_AUTH_URL=${ofAuthUrl#*=}
+    
+    accountClientId=$(grep 'ACCOUNT_CLIENT_ID' .env |  tr '\n' '\0')
+    ACCOUNT_CLIENT_ID=${accountClientId#*=}
+    
+    accountClientSecret=$(grep 'ACCOUNT_CLIENT_SECRET' .env |  tr '\n' '\0')
+    ACCOUNT_CLIENT_SECRET=${accountClientSecret#*=}
+    
+    
+    BASE_64=$(printf $ACCOUNT_CLIENT_ID:$ACCOUNT_CLIENT_SECRET | base64)
+    
+    #get account access token
+    echo "GET Account Access Token"
+    ACCESSTOKEN=$(curl --location --request POST $OF_AUTH_URL \
+        --header 'Content-Type: application/x-www-form-urlencoded' \
+        --header "Authorization: Basic $BASE_64" \
+    --data-urlencode 'grant_type=client_credentials' | jq -r '.access_token')
+    
+    # update account endpoint
+    echo "UPDATE Public Account Endpoint"
+    UPDATE_RESULT=$(curl --location --request PUT $OF_API_URL/a/settings \
+        --header "Authorization: Bearer $ACCESSTOKEN" \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+    "account_transaction_url": "'$URL'",
+    "auth_config": {"method": "X-API-KEY", "value": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"}
+    }' | jq -r '.account_transaction_url')
+    echo $UPDATE_RESULT
 }
 
 function networkUp() {
     networkDown
-    setupEnviroment
     docker-compose up --force-recreate -d 2>&1
     if [ $? -ne 0 ]; then
         echo "ERROR !!!! Unable to start network"
@@ -49,8 +52,8 @@ function networkUp() {
     fi
     
     sleep 1
-    echo "Sleeping 5s to allow ngrok docker to complete booting"
-    sleep 4
+    echo "Sleeping 3s to allow ngrok docker to complete booting"
+    sleep 2
     echo
     echo " ____    _____      _      ____    _____ "
     echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
@@ -58,10 +61,7 @@ function networkUp() {
     echo " ___) |   | |    / ___ \  |  _ <    | |  "
     echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
     echo
-    echo "Build your first sample done"
-    proxyAccountServer
-    # call update API
-
+    proxyAccountServer    
     echo "Open http://localhost:3000 on your browser"
 }
 
