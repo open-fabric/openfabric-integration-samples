499a0997cf47c8d92ed0cfc0495575714b075b5c
duonganhthu <duonganhthu43@gmail.com>
2022-03-02 18:39:12 +0700

    Update index.js

--- merchant-integration-samples/backend-flow-endpoint/index.js
+++ merchant-integration-samples/backend-flow-endpoint/index.js
@@ -67,7 +67,7 @@ const asyncRequest = (url, options) =>
   new Promise((resolve, reject) =>
     request(url, options, (error, res, body) => {
       if (!error && res.statusCode > 199 && res.statusCode < 300) {
-        resolve(body ? JSON.parse(body) : true);
+        resolve(body && !options.json ? JSON.parse(body) : body);
       } else {
         reject(
           error ?? `HTTP error: ${res.statusCode} ${JSON.stringify(res.body)}`
