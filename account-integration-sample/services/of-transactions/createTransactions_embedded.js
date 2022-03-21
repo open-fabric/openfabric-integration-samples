4feedf4b8181c48459fa5e2c46fc111471f47669
duonganhthu <duonganhthu43@gmail.com>
2022-03-21 18:31:40 +0700

    clean up readme

--- account-integration-sample/services/of-transactions/createTransactions_embedded.js
+++ account-integration-sample/services/of-transactions/createTransactions_embedded.js
@@ -1,20 +1,13 @@
 import axios from "axios";
-import {
-  of_api_url
-} from "../../lib/variables";
+import { of_api_url } from "../../lib/variables";
 import { GetAccessToken } from "../auth";
-export const createEmbeddedTransaction = async ({
-  transaction,
-}) => {
+export const createEmbeddedTransaction = async ({ transaction }) => {
   const { access_token } = await GetAccessToken();
-  const result = await axios.post(`${of_api_url}/t/transactions`,transaction,{
+  const result = await axios.post(`${of_api_url}/t/transactions`, transaction, {
     headers: {
       Authorization: `Bearer ${access_token}`,
       "Content-Type": "application/json",
     },
-  }).catch(err => {
-    console.log('=== err createEmbeddedTransaction', err)
-    throw new Error("OF Server - Embedded Flow return error: " + err.message)
-  })
-  return result.data
+  });
+  return result.data;
 };
