fdce950916568b1f19ae004a09aa9461d73d55b2
Viktor Belényesi <bvic23@gmail.com>
2022-05-03 17:43:00 +0200

    SP-1123: Add QR and deeplink support to account page
    f

--- account-integration-sample/controllers/orchestrated/ui.controller.js
+++ account-integration-sample/controllers/orchestrated/ui.controller.js
@@ -1,3 +1,4 @@
+import qr from "qrcode";
 import { catchAsync } from "../../utils/catchAsync";
 import { GetTransactionById } from "../../services/of-transactions";
 
@@ -9,8 +10,22 @@ export const CheckOutUI = catchAsync(async (req, res) => {
   const transInfo = await GetTransactionById({
     transaction_id: id,
   });
-  res.render("orchestrated/index", {
-    account_reference_id: transInfo?.account_reference_id,
-    id,
+
+  if (req.device.type !== 'desktop') {
+    res.render("orchestrated/index", {
+      account_reference_id: transInfo?.account_reference_id,
+      id,
+      deeplink: "openfabric://" + req.originalUrl
+    });
+  }
+
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  qr.toDataURL(fullUrl, (err, qrSRC) => {
+    res.render("orchestrated/index", {
+      account_reference_id: transInfo?.account_reference_id,
+      id,
+      qrSRC
+    });
   });
+
 });
