7ad2e99eb94a242a57fbfde3fbb7c502b1773db2
Viktor Belényesi <bvic23@gmail.com>
2022-05-06 18:36:49 +0200

    chore: Make dynamic schema example env independent

--- merchant-integration-sample/controllers/of-auth.ts
+++ merchant-integration-sample/controllers/of-auth.ts
@@ -1,9 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { OFAuthentication } from "../services/auth";
-export const Authentication = async (
+import type {NextApiRequest, NextApiResponse} from 'next'
+import {OFAuthentication} from "../services/auth";
+
+import {
+  merchant_client_secret,
+  merchant_client_id,
+} from "../lib/variables";
+
+export const Authentication = (id: string = merchant_client_id, secret: string = merchant_client_secret) => async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
-  const token = await OFAuthentication("resources/transactions.read resources/transactions.create");
+  const token = await OFAuthentication(id, secret)("resources/transactions.read resources/transactions.create");
   res.status(200).json(token);
 }
