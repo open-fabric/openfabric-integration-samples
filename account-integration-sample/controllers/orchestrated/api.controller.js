0ce40c8b06b24b468d114810b856eeeef348ae01
Viktor Belényesi <bvic23@gmail.com>
2022-09-26 15:19:41 +0200

    SP-1765: Revert changes and redirect back to success URL (#73)

--- account-integration-sample/controllers/orchestrated/api.controller.js
+++ account-integration-sample/controllers/orchestrated/api.controller.js
@@ -24,23 +24,6 @@ export const ApproveTransaction = catchAsync(async (req, res) => {
   return res.status(200).send(transInfo)
 });
 
-export const PreAuthTransaction = catchAsync(async (req, res) => {
-  const id = req.body?.id;
-  const approved_amount = req.body?.approved_amount
-  const approved_currency = req.body?.approved_currency
-  if (!id) {
-    throw Error("Missing of id in request");
-  }
-  await transactionService.PreAuthTransaction({
-    account_reference_id: req.body.account_reference_id,
-    approved_amount: approved_amount,
-    approved_currency: approved_currency
-  });
-  const transInfo = await transactionService.GetTransactionById({
-    transaction_id: id,
-  });
-  return res.status(200).send(transInfo)
-});
 
 export const mobileApproveTransaction = catchAsync(async (req, res) => {
   const id = req.body.id;
