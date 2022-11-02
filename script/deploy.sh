94204fb52b5299cdf8c35363d1281d4e4ddea07a
HoangNH <hoang.nguyen@itads.net>
2022-11-02 17:35:04 +0700

    chore: migrate to aws lightsail

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,58 +1,59 @@
 #!/bin/sh
 
-accountClientId=$(grep 'ACCOUNT_CLIENT_ID' .env |  tr '\n' '\0')
-ACCOUNT_CLIENT_ID=${accountClientId#*=}
+# accountClientId=$(grep 'ACCOUNT_CLIENT_ID' .env |  tr '\n' '\0')
+# ACCOUNT_CLIENT_ID=${accountClientId#*=}
 
-accountClientSecret=$(grep 'ACCOUNT_CLIENT_SECRET' .env |  tr '\n' '\0')
-ACCOUNT_CLIENT_SECRET=${accountClientSecret#*=}
+# accountClientSecret=$(grep 'ACCOUNT_CLIENT_SECRET' .env |  tr '\n' '\0')
+# ACCOUNT_CLIENT_SECRET=${accountClientSecret#*=}
 
-merchantClientId=$(grep 'MERCHANT_CLIENT_ID' .env |  tr '\n' '\0')
-MERCHANT_CLIENT_ID=${merchantClientId#*=}
+# merchantClientId=$(grep 'MERCHANT_CLIENT_ID' .env |  tr '\n' '\0')
+# MERCHANT_CLIENT_ID=${merchantClientId#*=}
 
-merchantClientSecret=$(grep 'MERCHANT_CLIENT_SECRET' .env |  tr '\n' '\0')
-MERCHANT_CLIENT_SECRET=${merchantClientSecret#*=}
+# merchantClientSecret=$(grep 'MERCHANT_CLIENT_SECRET' .env |  tr '\n' '\0')
+# MERCHANT_CLIENT_SECRET=${merchantClientSecret#*=}
 
-paymentGatewayPublishKey=$(grep 'PAYMENT_GATEWAY_PUBLISH_KEY' .env |  tr '\n' '\0')
-PAYMENT_GATEWAY_PUBLISH_KEY=${paymentGatewayPublishKey#*=}
+# paymentGatewayPublishKey=$(grep 'PAYMENT_GATEWAY_PUBLISH_KEY' .env |  tr '\n' '\0')
+# PAYMENT_GATEWAY_PUBLISH_KEY=${paymentGatewayPublishKey#*=}
 
-paymentGatewayName=$(grep 'PAYMENT_GATEWAY_NAME' .env |  tr '\n' '\0')
-PAYMENT_GATEWAY_NAME=${paymentGatewayName#*=}
+# paymentGatewayName=$(grep 'PAYMENT_GATEWAY_NAME' .env |  tr '\n' '\0')
+# PAYMENT_GATEWAY_NAME=${paymentGatewayName#*=}
 
-env=$(grep 'ENV' .env |  tr '\n' '\0')
-ENV=${env#*=}
+# env=$(grep 'ENV' .env |  tr '\n' '\0')
+# ENV=${env#*=}
 
-merchantServerUrl=$(grep 'MERCHANT_SERVER_URL' .env |  tr '\n' '\0')
-MERCHANT_SERVER_URL=${merchantServerUrl#*=}
+# merchantServerUrl=$(grep 'MERCHANT_SERVER_URL' .env |  tr '\n' '\0')
+# MERCHANT_SERVER_URL=${merchantServerUrl#*=}
 
-ofIssuerUrl=$(grep 'OF_ISSUER_URL' .env |  tr '\n' '\0')
-OF_ISSUER_URL=${ofIssuerUrl#*=}
+# ofIssuerUrl=$(grep 'OF_ISSUER_URL' .env |  tr '\n' '\0')
+# OF_ISSUER_URL=${ofIssuerUrl#*=}
 
-ofAuthUrl=$(grep 'OF_AUTH_URL' .env |  tr '\n' '\0')
-OF_AUTH_URL=${ofAuthUrl#*=}
+# ofAuthUrl=$(grep 'OF_AUTH_URL' .env |  tr '\n' '\0')
+# OF_AUTH_URL=${ofAuthUrl#*=}
 
-ofAPIUrl=$(grep 'OF_API_URL' .env |  tr '\n' '\0')
-OF_API_URL=${ofAPIUrl#*=}
+# ofAPIUrl=$(grep 'OF_API_URL' .env |  tr '\n' '\0')
+# OF_API_URL=${ofAPIUrl#*=}
 
-accountServerUrl=$(grep 'ACCOUNT_SERVER_URL' .env |  tr '\n' '\0')
-ACCOUNT_SERVER_URL=${accountServerUrl#*=}
+# accountServerUrl=$(grep 'ACCOUNT_SERVER_URL' .env |  tr '\n' '\0')
+# ACCOUNT_SERVER_URL=${accountServerUrl#*=}
 
-# build image
-docker build ./nginx/ -t openfabric-integration-samples-nginx:latest
-docker build ./merchant-integration-sample/ \
-  -t openfabric-integration-samples-merchant:latest \
-  --build-arg ACCOUNT_CLIENT_ID=$ACCOUNT_CLIENT_ID \
-  --build-arg ACCOUNT_CLIENT_SECRET=$ACCOUNT_CLIENT_SECRET \
-  --build-arg MERCHANT_CLIENT_ID=$MERCHANT_CLIENT_ID \
-  --build-arg MERCHANT_CLIENT_SECRET=$MERCHANT_CLIENT_SECRET \
-  --build-arg PAYMENT_GATEWAY_PUBLISH_KEY=$PAYMENT_GATEWAY_PUBLISH_KEY \
-  --build-arg PAYMENT_GATEWAY_NAME=$PAYMENT_GATEWAY_NAME \
-  --build-arg ENV=$ENV \
-  --build-arg MERCHANT_SERVER_URL=$MERCHANT_SERVER_URL \
-  --build-arg OF_ISSUER_URL=$OF_ISSUER_URL \
-  --build-arg OF_AUTH_URL=$OF_AUTH_URL \
-  --build-arg OF_API_URL=$OF_API_URL \
-  --build-arg ACCOUNT_SERVER_URL=$ACCOUNT_SERVER_URL
-docker build ./account-integration-sample/ -t openfabric-integration-samples-account:latest
+# # build image
+# docker build ./nginx/ -t openfabric-integration-samples-nginx:latest
+# docker build ./merchant-integration-sample/ \
+#   -t openfabric-integration-samples-merchant:latest \
+#   --build-arg ACCOUNT_CLIENT_ID=$ACCOUNT_CLIENT_ID \
+#   --build-arg ACCOUNT_CLIENT_SECRET=$ACCOUNT_CLIENT_SECRET \
+#   --build-arg MERCHANT_CLIENT_ID=$MERCHANT_CLIENT_ID \
+#   --build-arg MERCHANT_CLIENT_SECRET=$MERCHANT_CLIENT_SECRET \
+#   --build-arg PAYMENT_GATEWAY_PUBLISH_KEY=$PAYMENT_GATEWAY_PUBLISH_KEY \
+#   --build-arg PAYMENT_GATEWAY_NAME=$PAYMENT_GATEWAY_NAME \
+#   --build-arg ENV=$ENV \
+#   --build-arg MERCHANT_SERVER_URL=$MERCHANT_SERVER_URL \
+#   --build-arg OF_ISSUER_URL=$OF_ISSUER_URL \
+#   --build-arg OF_AUTH_URL=$OF_AUTH_URL \
+#   --build-arg OF_API_URL=$OF_API_URL \
+#   --build-arg ACCOUNT_SERVER_URL=$ACCOUNT_SERVER_URL
+# docker build ./account-integration-sample/ -t openfabric-integration-samples-account:latest
+# docker images
 
 # REGEX to separate image name after deploy
 REGEX='Refer to this image as \"(.*)\" in deployments.'
@@ -62,14 +63,16 @@ for name in "NGINX" "MERCHANT" "ACCOUNT"
 do
   echo "push $name"
   lower_case_name=$(echo "${name}" | tr '[:upper:]' '[:lower:]')
+  echo $lower_case_name
 
   DESCRIPTION=$(aws lightsail push-container-image --region ap-southeast-1 --service-name account-merchant-sample --label openfabric-integration-samples-${lower_case_name} --image openfabric-integration-samples-${lower_case_name}:latest)
+  echo $DESCRIPTION
 
   if [[ $DESCRIPTION =~ $REGEX ]]
   then
     IMAGE_NAME="${BASH_REMATCH[1]}"
   fi
-  echo "Nginx image name: $IMAGE_NAME"
+  echo "Image name: $IMAGE_NAME"
 
   export $name"_IMAGE_NAME"="$IMAGE_NAME"
 done
