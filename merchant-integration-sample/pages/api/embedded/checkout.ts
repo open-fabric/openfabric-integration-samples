1550aad692b9e6b2830a2935b913a4a7c4e4cfc7
hoangnh93 <hoangnguyenhong1993@gmail.com>
2022-11-29 14:52:02 +0700

    fix: remove old merchant integration

--- merchant-integration-sample/pages/api/embedded/checkout.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { EmbeddedCheckout } from '../../../controllers/embedded';
-
-const handler = async (req: NextApiRequest,
-   res: NextApiResponse) => {
- return EmbeddedCheckout(req,res)
-}
-export default handler
