b36b0e0e77c097b95cc5e9897af06b2b012c1d96
loimai <huuloiofficial@gmail.com>
2023-03-23 15:52:49 +0700

    style: make text bigger

--- pat-link-sample/src/pages/approval_result.tsx
+++ pat-link-sample/src/pages/approval_result.tsx
@@ -1,7 +1,10 @@
 import {useSearchParams} from "next/navigation";
+import styles from '@/styles/Home.module.css'
 
-export default function ApprovalResult (){
+
+export default function ApprovalResult() {
     const searchParams = useSearchParams();
     let status = searchParams.get("status");
-    return <div><label>Your subscription approval is </label> <b>{status}</b></div>
+    const message = status == 'approved' ? "You purchase is successful" : "You purchases is denied"
+    return <div className={styles.center}><h1>{message}</h1></div>
 }
