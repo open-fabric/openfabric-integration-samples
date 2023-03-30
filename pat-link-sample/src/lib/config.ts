818a7588a78205bad7c1ec61ac92b49bf917873f
loimai <huuloiofficial@gmail.com>
2023-03-30 14:39:24 +0700

    chore: configure default url

--- pat-link-sample/src/lib/config.ts
+++ pat-link-sample/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const OF_AUTH_ENDPOINT = `${process.env.OF_AUTH_ENDPOINT}` || 'https://auth.dev.openfabric.co'
-export const OF_API_URL = process.env.OF_API_URL || 'https://api.dev.openfabric.co'
+export const OF_API_URL = `${process.env.OF_API_URL}` || 'https://api.dev.openfabric.co'
 export const MERCHANT_SAMPLE_HOST = `${process.env.MERCHANT_SAMPLE_HOST}` || 'https://merchant-1.samples.dev.openfabric.co'
 export const MERCHANT_CLIENT_ID = `${process.env.MERCHANT_CLIENT_ID}`
 export const MERCHANT_CLIENT_SECRET = `${process.env.MERCHANT_CLIENT_SECRET}`
