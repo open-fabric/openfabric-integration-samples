02c4e714effd9ab882bc545723f6af89a01207af
loimai <huuloiofficial@gmail.com>
2023-04-04 09:01:33 +0700

    chores: remove logs

--- pat-link-sample/src/pages/api/link.ts
+++ pat-link-sample/src/pages/api/link.ts
@@ -22,10 +22,6 @@ export default async function handler(
   const token = await authenticate()
 
   const domain = req.headers?.origin
-  console.log('req.headers')
-  console.log(req.headers.origin)
-  console.log(req.headers.host)
-  console.log(JSON.stringify(req.headers))
   let partnerRedirectUrl = `${domain}/merchant-pat-link/approval_result`;
   var days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat','sun'];
 
