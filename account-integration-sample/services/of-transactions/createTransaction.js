8ea6020858546a4eb37c9930d7a4d6e281f8f3f6
Vinh Bach Sy Duc <886778+vinh0604@users.noreply.github.com>
2023-04-28 12:49:35 +0700

    SP-2305: add reason for instant decline scenario

--- account-integration-sample/services/of-transactions/createTransaction.js
+++ account-integration-sample/services/of-transactions/createTransaction.js
@@ -1,35 +1,33 @@
 import { v4 as uuid } from "uuid";
-import {
-  account_server_url
-} from "../../lib/variables";
-export const CreateTransaction = async ({
-  of_transaction
-}) => {
+import { account_server_url } from "../../lib/variables";
+export const CreateTransaction = async ({ of_transaction }) => {
   return {
     account_reference_id: uuid(),
     // url for customer redirect to account system
-    payment_redirect_web_url: `${account_server_url}/orchestrated/checkout?id=${of_transaction.fabric_reference_id ||
-      of_transaction.id}`,
-  }
+    payment_redirect_web_url: `${account_server_url}/orchestrated/checkout?id=${
+      of_transaction.fabric_reference_id || of_transaction.id
+    }`,
+  };
 };
 
-export const CreatePreApprovedTransaction = async ({
-                                          of_transaction
-                                        }) => {
+export const CreatePreApprovedTransaction = async ({ of_transaction }) => {
   const amount = of_transaction.amount;
-  let status = null;
-  if (amount < 100 ) {
-    status = 'approved'
+  let status;
+  let reason;
+  if (amount < 100) {
+    status = "approved";
   } else if (amount > 150) {
-    status = 'failed'
+    status = "failed";
+    reason = "Not credit enough balance";
   }
 
   return {
     account_reference_id: uuid(),
     // url for customer redirect to account system
-    payment_redirect_web_url: `${account_server_url}/orchestrated/checkout?id=${of_transaction.fabric_reference_id ||
-    of_transaction.id}`,
-    status: status
-  }
+    payment_redirect_web_url: `${account_server_url}/orchestrated/checkout?id=${
+      of_transaction.fabric_reference_id || of_transaction.id
+    }`,
+    status: status,
+    reason: reason,
+  };
 };
-
