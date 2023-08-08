bb5f904b9181f37e12156c4969b153d119b7a549
Tung Dao <me@tungdao.com>
2023-08-08 15:51:56 +0700

    add endpoint GET /api/unilateral/partners

--- account-integration-sample/controllers/unilateral/api.controller.js
+++ account-integration-sample/controllers/unilateral/api.controller.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { of_issuer_url } from "../../lib/variables.js";
+import { of_issuer_url, of_auth_url } from "../../lib/variables.js";
 import { GetAccessToken } from "../../services/auth.js";
 import { catchAsync } from "../../utils/catchAsync";
 import * as transactionService from "../../services/of-transactions";
@@ -35,3 +35,44 @@ export const FetchCard = async (card_fetch_token, access_token) => {
     );
     return result.data;
 };
+
+export const MERCHANTS = [{
+  name: 'Lazada',
+  url: 'https://www.lazada.sg',
+  clientId: '5gooe22dkk23f3nheika6p212',
+  clientSecret: '1816cbmasgd86usjcsl4eu65lns8bd3ul1938bcg2nsdkj39ui5r'
+}]
+
+export const authorize = async (clientId, clientSecret, scope) => {
+  const response = await fetch(
+    of_auth_url, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        grant_type: 'client_credentials',
+      })
+    }
+  )
+  console.log(response.status)
+  const data = await response.json()
+  return data.access_token
+}
+
+export const listPartners = catchAsync(async (req, res) => {
+  const data = await Promise.all(
+    MERCHANTS.map(async ({ clientId, clientSecret, ...merchant }) => {
+      const accessToken = await authorize(
+        clientId, clientSecret,
+        'resources/transactions.create'
+      )
+      return {
+        ...merchant,
+        accessToken,
+      }
+    })
+  )
+  res.json(data)
+})
