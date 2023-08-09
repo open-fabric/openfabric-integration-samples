ca2dabca79ccb3af0bcfdca935d31b27c8d649de
Tung Dao <me@tungdao.com>
2023-08-09 10:33:30 +0700

    add endpoint GET /api/unilateral/partners (#125)
    * add endpoint GET /api/unilateral/partners
    
    * add: support for both sandbox and dev credentials

--- account-integration-sample/routes/api/unilateral.route.js
+++ account-integration-sample/routes/api/unilateral.route.js
@@ -1,5 +1,9 @@
 import express from "express";
 import { unilateralController } from "../../controllers";
+
 const router = express.Router();
+
 router.route("/transactions").post(unilateralController.CreateTransaction);
+router.route("/partners").get(unilateralController.listPartners);
+
 export const Unilateral = router;
