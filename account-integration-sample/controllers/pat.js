2232d21a39abfdab45b0194007057728d892fe29
Vinh Bachsy <886778+vinh0604@users.noreply.github.com>
2023-11-03 16:44:59 +0800

    SP-3196: Update tenant response for PAT (#142)

--- account-integration-sample/controllers/pat.js
+++ account-integration-sample/controllers/pat.js
@@ -2,7 +2,7 @@ import crypto from 'crypto'
 
 import { db } from '../db/index.js'
 import { catchAsync } from '../utils/catchAsync.js';
-import {account_server_url, of_api_url, of_gateway_redirect_url} from "../lib/variables.js";
+import {account_server_url, of_api_url} from "../lib/variables.js";
 import axios from "axios";
 import {GetAccessToken} from "../services/auth.js";
 
@@ -16,22 +16,23 @@ export const create = catchAsync(async (req, res) => {
     intent: req.body.intent,
     description: req.body.description,
     constraints: req.body.constraints,
-    of_link_ref: req.body.of_link_ref,
-    of_gateway_redirect_url: req.body.of_gateway_redirect_url
+    of_link_id: req.body.link_id,
+    of_gateway_redirect_url: req.body.gateway_redirect_url
   })
   res.send({
     tenant_link_ref: ref,
     tenant_customer_ref: customerId,
-    consent_capture_page_url: new URL(`/pat/consent/${ref}`, account_server_url).toString()
+    tenant_consent_redirect_url: new URL(`/pat/consent/${ref}`, account_server_url).toString()
   })
 })
 
 export const consentCapturePage = catchAsync(async (req, res) => {
   try {
     const {access_token} = await GetAccessToken()
+    const patLinkData = db.getData(`/pat_links/${req.params.id}`)
 
     const result = await axios.get(
-      new URL(`/v1/preapproved_transaction_links/${req.params.id}`, of_api_url).toString(),
+      new URL(`/v1/preapproved_transaction_links/${patLinkData.link_id}`, of_api_url).toString(),
       {
         headers: {
           Authorization: `Bearer ${access_token}`,
@@ -49,8 +50,8 @@ export const consentCapturePage = catchAsync(async (req, res) => {
 
 export const approvePatLink = catchAsync(async (req, res) => {
   const {access_token} = await GetAccessToken()
-  const data = req.body;
-  const status = data.status;
+  const patLinkData = db.getData(`/pat_links/${req.params.id}`)
+  const status = req.body.status;
   let reason;
   if(status == "approved") {
     reason = "Card is valid for purchasing";
@@ -60,8 +61,7 @@ export const approvePatLink = catchAsync(async (req, res) => {
   await axios.patch(
     new URL("/v1/preapproved_transaction_links", of_api_url).toString(),
     {
-      id: data.id,
-      tenant_link_ref: data.tenant_link_ref,
+      tenant_link_ref: patLinkData.id,
       reason: reason,
       status: status,
     },
@@ -75,7 +75,7 @@ export const approvePatLink = catchAsync(async (req, res) => {
 
   res.send(
     {
-      url: `${of_gateway_redirect_url}?link_id=${data.id}`
+      url: patLinkData.of_gateway_redirect_url,
     }
   );
 
