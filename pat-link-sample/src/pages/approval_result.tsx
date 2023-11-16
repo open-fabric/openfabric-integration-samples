231f6479f95786a3777935dd8c1dca6557963681
Vinh Bach Sy Duc <886778+vinh0604@users.noreply.github.com>
2023-11-16 13:52:41 +0800

    SP-3221: fix result page

--- pat-link-sample/src/pages/approval_result.tsx
+++ pat-link-sample/src/pages/approval_result.tsx
@@ -4,9 +4,9 @@ import styles from '@/styles/Home.module.css'
 
 export default function ApprovalResult() {
     const searchParams = useSearchParams();
-    const status = searchParams.get("pat_status");
-    const linkId = searchParams.get("pat_link_id");
-    const partnerLinkRef = searchParams.get("pat_partner_link_ref");
+    const status = searchParams.get("status");
+    const linkId = searchParams.get("link_id");
+    const partnerLinkRef = searchParams.get("partner_link_ref");
     return <div className={styles.center}>
         <h1>You transaction is {status}</h1>
         <h2>Link ID: {linkId}</h2>
