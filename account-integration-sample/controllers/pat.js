c1972e62f1d180fa923fe1f1f28e5c8f8827ccb8
abrahamoed <abraham@openfabric.co>
2023-11-21 15:25:31 +0800

    SP-3237 tenant init pat link pages

--- account-integration-sample/controllers/pat.js
+++ account-integration-sample/controllers/pat.js
@@ -1,29 +1,57 @@
 import crypto from 'crypto'
 
-import { db } from '../db/index.js'
 import { catchAsync } from '../utils/catchAsync.js';
-import {account_server_url, of_api_url} from "../lib/variables.js";
+import {of_api_url} from "../lib/variables.js";
 import axios from "axios";
 import {GetAccessToken} from "../services/auth.js";
 
+// update to hit create pat endpoint
 export const create = catchAsync(async (req, res) => {
-  const ref = crypto.randomUUID()
-  const customerId = crypto.randomUUID()
-  await db.push(`/pat_links/${ref}`, {
-    id: ref,
-    partner_link_ref: req.body.partner_link_ref,
-    partner_customer_ref: req.body.partner_customer_ref,
-    intent: req.body.intent,
-    description: req.body.description,
-    constraints: req.body.constraints,
-    of_link_id: req.body.link_id,
-    of_gateway_redirect_url: req.body.gateway_redirect_url
-  })
-  res.send({
-    tenant_link_ref: ref,
-    tenant_customer_ref: customerId,
-    tenant_consent_redirect_url: new URL(`/pat/consent/${ref}`, account_server_url).toString()
-  })
+  const data = req.body;
+
+  try {
+    const {access_token} = await GetAccessToken()
+
+    const result = await axios.post(
+      new URL(`/v1/preapproved_transaction_links`, of_api_url).toString(),
+      {
+        tenant_link_ref: data.tenant_link_ref,
+        tenant_partner_ref: data.tenant_partner_ref,
+        tenant_customer_ref: data.tenant_customer_ref,
+        return_url: data.returl_url,
+        description: data.description,
+        constraints: {
+          currency: data.currency,
+          start_date: new Date().toISOString(),
+        },
+        customer_info: {
+          first_name: data.first_name,
+          last_name: data.last_name,
+          email: data.email,
+          mobile_number: data.mobile_number
+        },
+        billing_address: {
+          address_line_1: data.address_line_1,
+          address_line_2: data.address_line_2,
+          city: data.city,
+          country: data.country,
+          post_code: data.post_code
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.send({
+      url: result.gateway_redirect_url
+    })
+  } catch (e) {
+    res.status(404).send('Unable to create')
+  }
 })
 
 export const consentCapturePage = catchAsync(async (req, res) => {
@@ -47,6 +75,26 @@ export const consentCapturePage = catchAsync(async (req, res) => {
     console.error('Failed to fetch PAT link data', e);
     res.status(404).send('Not found')
   }
+})
+
+export const createPatPage = catchAsync(async (req, res) => {
+  res.render("pat/create", {
+    tenant_link_ref: crypto.randomUUID(),
+    tenant_partner_ref: crypto.randomUUID(),
+    tenant_customer_ref: crypto.randomUUID(),
+  });
+})
+
+export const createPatSuccessPage = catchAsync(async (req, res) => {
+  const query = req.query
+  res.render("pat/create_success", {
+    data: {
+      link_id: query.link_id,
+      partner_link_ref: query.partner_link_ref,
+      tenant_link_ref: query.tenant_link_ref,
+      status: query.status,
+    }
+  })
 })
 
 export const approvePatLink = catchAsync(async (req, res) => {
--- account-integration-sample/routes/pat.routes.js
+++ account-integration-sample/routes/pat.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import {create, consentCapturePage, approvePatLink} from '../controllers/pat.js'
+import {create, consentCapturePage, approvePatLink, createPatPage, createPatSuccessPage} from '../controllers/pat.js'
 
 const router = express.Router();
 
 router.route('/api/pat').post(create);
-router.route('/pat/consent/:id').get(consentCapturePage);
 router.route('/api/pat/:id/approve').patch(approvePatLink);
+router.route('/pat/consent/:id').get(consentCapturePage);
+router.route('/pat/create').get(createPatPage);
+router.route('/pat/create_success').get(createPatSuccessPage)
 
 export default router;
