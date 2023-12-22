6a38b194d3fb148f83d347b9ab34b6a462b6b797
Hoang Nguyen Hong <hoangnguyenhong1993@gmail.com>
2023-12-22 16:23:19 +0700

    SP-3149: add pba tenant approval and webhook (#183)
    * SP-3149: add pba tenant approval and webhook
    
    * SP-3149: update random
    
    * SP-3149: update message
    
    * SP-3149: address comments
    
    ---------
    
    Co-authored-by: root <hoang@openfabric.co>

--- account-integration-sample/controllers/pba/api.controller.js
+++ account-integration-sample/controllers/pba/api.controller.js
@@ -1,15 +1,16 @@
 import { catchAsync } from '../../utils/catchAsync.js';
-import {of_api_url} from "../../lib/variables.js";
+import { of_api_url } from "../../lib/variables.js";
 import axios from "axios";
-import {GetAccessToken} from "../../services/auth.js";
-import { db } from '../../db/index.js';
+import { GetAccessToken } from "../../services/auth.js";
+import { db, addNewPbaTransactions } from '../../db/index.js';
+import { trusted_api_key } from "../../lib/variables.js";
 
 export const provisionAccountDevice = catchAsync(async (req, res) => {
-  const {access_token} = await GetAccessToken('resources/customers.create')
+  const { access_token } = await GetAccessToken('resources/customers.create')
   // read header X-User-Id from Express request
   const userId = req.headers["x-user-id"];
-  const tenantCustomerRef = "C0"+userId;
-  const financialAccountNumber = "FR"+userId;
+  const tenantCustomerRef = "C0" + userId;
+  const financialAccountNumber = "FR" + userId;
   const data = req.body;
 
   const reqBody = {
@@ -31,7 +32,7 @@ export const provisionAccountDevice = catchAsync(async (req, res) => {
   );
 
   db.push(`/pba/customers/${tenantCustomerRef}`, {
-    data:result.data,
+    data: result.data,
   })
 
   res.send(
@@ -41,4 +42,68 @@ export const provisionAccountDevice = catchAsync(async (req, res) => {
     }
   );
 });
+
+export const approveFinalAuthTransaction = catchAsync(async (req, res) => {
+  const tenantReferenceId = uuidv4()
+  let approveAmount = req.data.amount;
+  let reason;
+  let status = 'approved';
+
+  if (req.data.amount > 8000) {
+    approveAmount = 0;
+    status = 'declined';
+    reason = 'Fail for transaction amount over 8000';
+  } else {
+    if (req.data.auth_indicator?.is_partial_approval) {
+      approveAmount -= getRandomNumber(1, Math.floor(req.data.amount));
+    }
+  }
+
+
+  const transaction = {
+    ...req.data,
+    tenant_reference_id: tenantReferenceId,
+    approved_amount: approveAmount,
+    status: status,
+    reason: reason,
+  }
+
+  addNewPbaTransactions(transaction);
+
+  res.send(
+    {
+      tenant_reference_id: transaction.tenant_reference_id,
+      approved_amount: transaction.approved_amount,
+      status: transaction.status,
+      reason: transaction.reason,
+    }
+  );
+});
+
+export const WebhookCallBack = catchAsync(async (req, res) => {
+  if (req.header("X-Api-Key") === trusted_api_key || req.header("X-API-Key") === trusted_api_key) {
+    console.log(
+      "Received OF notifications: ",
+      JSON.stringify(req.body, null, 2)
+    );
+
+    if (Array.isArray(req.body)) {
+      req.body.forEach((notification) => {
+        console.log(
+          `Pba transaction notification: id=${notification.tenant_reference_id}, of_transaction_id=${notification.transaction_id}, notification_id=${notification.notification_id}`
+        );
+      });
+    }
+
+    return res.status(200).send({ status: "Success" });
+  } else {
+    return res
+      .status(401)
+      .send({ status: "Failed", reason: "Unauthenticated" });
+  }
+});
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
--- account-integration-sample/routes/api/pba.route.js
+++ account-integration-sample/routes/api/pba.route.js
@@ -2,5 +2,6 @@ import express from "express";
 import { pbaController } from "../../controllers/index.js";
 const router = express.Router();
 router.route("/provision").post(pbaController.provisionAccountDevice);
-
+router.route("/transactions/final-auth/approval").post(pbaController.approveFinalAuthTransaction);
+router.route("/callback").post(pbaController.WebhookCallBack);
 export const PBA = router;
