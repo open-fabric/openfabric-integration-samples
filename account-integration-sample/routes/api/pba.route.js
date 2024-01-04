6db52286d88be74c2abd5ff0580c909803452233
abrahamoed <abraham@openfabric.co>
2024-01-04 15:58:01 +0800

    SP-3358 change webhook endpoint

--- account-integration-sample/routes/api/pba.route.js
+++ account-integration-sample/routes/api/pba.route.js
@@ -3,5 +3,5 @@ import { pbaController } from "../../controllers/index.js";
 const router = express.Router();
 router.route("/provision").post(pbaController.provisionAccountDevice);
 router.route("/transactions/final-auth/approval").post(pbaController.approveFinalAuthTransaction);
-router.route("/callback").post(pbaController.WebhookCallBack);
+router.route("/webhook").post(pbaController.WebhookCallBack);
 export const PBA = router;
